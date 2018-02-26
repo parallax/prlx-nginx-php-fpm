9118f290cbc8e4e5fe48975582f736c6b42ff171
Lawrence Dudley <lawrence@parall.ax>
2018-02-26 10:38:17 +0000

    add version output

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+echo ' ____   __    ____    __    __    __      __    _  _ 
+(  _ \ /__\  (  _ \  /__\  (  )  (  )    /__\  ( \/ )
+ )___//(__)\  )   / /(__)\  )(__  )(__  /(__)\  )  ( 
+(__) (__)(__)(_)\_)(__)(__)(____)(____)(__)(__)(_/\_)'
+
+# Version numbers:
+/usr/sbin/nginx -v
+/usr/bin/php -v
+/usr/bin/php -m
+
 # Set the newrelic app name programatically with the info we already have in the container
 sed -i -e "s/newrelic.appname = \"PHP Application\"/newrelic.appname = \"${SITE_NAME}-${SITE_BRANCH}-${ENVIRONMENT}\"/g" /etc/php7/conf.d/newrelic.ini
 
