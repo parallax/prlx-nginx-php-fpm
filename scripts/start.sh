77ea90dd19f17c6b2912424fae2df9eb72000d62
Lawrence Dudley <lawrence@parall.ax>
2018-03-08 12:32:40 +0000

    add php5.6 dockerfile

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,7 +10,7 @@ echo ' ____   __    ____    __    __    __      __    _  _
 /usr/bin/php -v
 
 # Set the newrelic app name programatically with the info we already have in the container
-sed -i -e "s/newrelic.appname = \"PHP Application\"/newrelic.appname = \"${SITE_NAME}-${SITE_BRANCH}-${ENVIRONMENT}\"/g" /etc/php7/conf.d/newrelic.ini
+sed -i -e "s/newrelic.appname = \"PHP Application\"/newrelic.appname = \"${SITE_NAME}-${SITE_BRANCH}-${ENVIRONMENT}\"/g" /etc/php/conf.d/newrelic.ini
 
 # Start supervisord and services
 exec /usr/bin/supervisord -n -c /etc/supervisord.conf
